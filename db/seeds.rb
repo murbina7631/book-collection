e785024c8c7d639e2156f9c444b0d66dd82ce07c
murbina7631 <u.miguel.j@gmail.com>
2022-10-13 23:39:03 -0500

    Populate Heroku Database
    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create!([
+  {title: "A Man & His Cat", author: "Umi Sakurai", price: "12.99", published_date: "2022-10-13"},
+  {title: "Aposimz", author: "Tsutomu Nihei", price: "12.95", published_date: "2022-10-13"},
+  {title: "A Silent Voice", author: "Yoshitoki Oima", price: "10.99", published_date: "2022-10-13"}
+])
